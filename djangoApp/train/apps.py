7f241c807b5659f9b7c22a6834d17ea564d75659
DANLENEHAN <daniel.lenehan@ucdconnect.ie>
2019-08-14 13:29:45 +0100

    User Profile Update capability

--- djangoApp/train/apps.py
+++ djangoApp/train/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class TrainConfig(AppConfig):
     name = 'train'
 
+    def ready(self):
+        # Pycharm does'nt recognize but it works
+        import train.signals
